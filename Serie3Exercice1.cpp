45ee6041777cce9198d24684ee746dc5b9b51dc4
aymen <aymen.sellaouti@gmail.com>
2019-10-10 16:54:29 +0100

    =aout exercice 1 serie 3 mise a jour 2

--- Serie3Exercice1.cpp
+++ Serie3Exercice1.cpp
@@ -1,3 +1,4 @@
+//https://github.com/aymensellaouti/tdCetExemplesCours
 /**
 fonction estCroissante(n : entier) : booleen
 var 
@@ -9,7 +10,34 @@ debut
 		n <- n div 10
 	fintanque
 	retourner(vrai)
-fin 
+fin
+**/ 
 #include<stdio.h>
+int estCroissante(int n) {
+	while(n>9) {
+		if ((n % 10) < ((n / 10) % 10)) {
+			printf("Problème : %d est < à %d \n", n%10, (n / 10) % 10);
+			return (0);			
+		}
+		n = n / 10;		
+	}
+	return(1);	
+} 
+
+int main() {
+	int n;
+	do {
+		printf("Donner un entier : ");
+		scanf("%d",&n);		
+	} while(n<=0);
+	if(estCroissante(n)){
+		printf("Le nombre %d est croissant :D", n);
+	}else{
+		printf("Le nombre %d n'est pas croissant :(", n);
+	}
+}
+
+
+
 
 
